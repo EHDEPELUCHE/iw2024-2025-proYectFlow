a0a3c6081e1184336178821d35ee2d3f5368d024
Álvaro Pérez Vargas <80173076+EHDEPELUCHE@users.noreply.github.com>
2024-10-17 15:58:46 +0200

    Creación de un proyecto.
    He creado el proyecto como proyecto de java para maven. Nos deseo un muy buen proyecto a todos.

---
+++ source/proYectFlow/src/main/java/es/uca/iw/Main.java
@@ -0,0 +1,8 @@
+package es.uca.iw;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Muy buenos días a todos, hoy será un gran día.");
+    }
+}
