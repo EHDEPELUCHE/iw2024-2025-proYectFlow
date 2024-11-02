9ac07879aa89cb84b391e19b5fc2614b183e4388
Marta <marta.villalopez@alum.uca.es>
2024-11-03 00:41:19 +0100

    Proyecto usando vaadin

---
+++ source/proyectflow/src/main/java/es/uca/iw/views/iniciosesión/InicioSesiónView.java
@@ -0,0 +1,34 @@
+package es.uca.iw.views.iniciosesión;
+
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@PageTitle("Inicio Sesión")
+@Route("inicio-sesion")
+@Menu(order = 2, icon = "line-awesome/svg/user.svg")
+public class InicioSesiónView extends Composite<VerticalLayout> {
+
+    public InicioSesiónView() {
+        VerticalLayout layoutColumn2 = new VerticalLayout();
+        LoginForm loginForm = new LoginForm();
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        getContent().setJustifyContentMode(JustifyContentMode.START);
+        getContent().setAlignItems(Alignment.CENTER);
+        layoutColumn2.setWidth("100%");
+        layoutColumn2.setMaxWidth("800px");
+        layoutColumn2.setHeight("min-content");
+        layoutColumn2.setAlignSelf(FlexComponent.Alignment.CENTER, loginForm);
+        //loginForm.setWidth("350px");
+        //loginForm.setHeight("406px");
+        getContent().add(layoutColumn2);
+        layoutColumn2.add(loginForm);
+    }
+}
---
+++ source/proyectflow/src/main/java/es/uca/iw/views/registrodeusuario/RegistrodeusuarioView.java
@@ -0,0 +1,77 @@
+package es.uca.iw.views.registrodeusuario;
+
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+
+@PageTitle("Registro de usuario")
+@Route("registro-usuario")
+@Menu(order = 1, icon = "line-awesome/svg/user.svg")
+public class RegistrodeusuarioView extends Composite<VerticalLayout> {
+
+    public RegistrodeusuarioView() {
+        VerticalLayout layoutColumn2 = new VerticalLayout();
+        H3 h3 = new H3();
+        FormLayout formLayout2Col = new FormLayout();
+        TextField textField = new TextField();
+        TextField textField2 = new TextField();
+        EmailField emailField = new EmailField();
+        TextField textField3 = new TextField();
+        PasswordField passwordField = new PasswordField();
+        PasswordField passwordField2 = new PasswordField();
+        HorizontalLayout layoutRow = new HorizontalLayout();
+        Button buttonPrimary = new Button();
+        Button buttonSecondary = new Button();
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        getContent().setJustifyContentMode(JustifyContentMode.START);
+        getContent().setAlignItems(Alignment.CENTER);
+        layoutColumn2.setWidth("100%");
+        layoutColumn2.setMaxWidth("800px");
+        layoutColumn2.setHeight("min-content");
+        h3.setText("Registro de usuario");
+        h3.setWidth("100%");
+        formLayout2Col.setWidth("100%");
+        textField.setLabel("Nombre");
+        textField2.setLabel("Apellidos");
+        emailField.setLabel("Email");
+        textField3.setLabel("Teléfono");
+        passwordField.setLabel("Contraseña");
+        passwordField.setWidth("min-content");
+        passwordField2.setLabel("Repetir contraseña");
+        passwordField2.setWidth("min-content");
+        layoutRow.addClassName(Gap.MEDIUM);
+        layoutRow.setWidth("100%");
+        layoutRow.getStyle().set("flex-grow", "1");
+        buttonPrimary.setText("Save");
+        buttonPrimary.setWidth("min-content");
+        buttonPrimary.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        buttonSecondary.setText("Cancel");
+        buttonSecondary.setWidth("min-content");
+        getContent().add(layoutColumn2);
+        layoutColumn2.add(h3);
+        layoutColumn2.add(formLayout2Col);
+        formLayout2Col.add(textField);
+        formLayout2Col.add(textField2);
+        formLayout2Col.add(emailField);
+        formLayout2Col.add(textField3);
+        formLayout2Col.add(passwordField);
+        formLayout2Col.add(passwordField2);
+        layoutColumn2.add(layoutRow);
+        layoutRow.add(buttonPrimary);
+        layoutRow.add(buttonSecondary);
+    }
+}
