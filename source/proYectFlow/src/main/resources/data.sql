c586cb388468b60b9240e02e5ed0bac87f9a9d9f
Álvaro Pérez Vargas <80173076+EHDEPELUCHE@users.noreply.github.com>
2024-12-05 15:30:52 +0100

    Añadidos promotores ejemplo
    Se cargan automáticamente al arranque si no tienes base de datos persistente.

---
+++ source/proYectFlow/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+insert into usuario(id, version, username, nombre, apellido, contrasenna, correo, tipo)
+values ("promotor1", 1, 'Promocioneitor', 'Yono', 'Teabalo', 'estoyCifrada', 'promotor1@g.com', 'PROMOTOR');
+insert into usuario(id, version, username, nombre, apellido, contrasenna, correo, tipo)
+values ("promotor2", 1, 'Promocionador', 'Yosi', 'Teabalo', 'estoyCifrada', 'promotor2@g.com', 'PROMOTOR');
+insert into usuario(id, version, username, nombre, apellido, contrasenna, correo, tipo)
+values ("promotor3", 1, 'Promocionista', 'Cualquiera', 'Teabala', 'estoyCifrada', 'promotor3@g.com', 'PROMOTOR');
+insert into usuario(id, version, username, nombre, apellido, contrasenna, correo, tipo)
+values ("promotor4", 1, 'Promocionester', 'Emilio', 'Delgado', 'estoyCifrada', 'promotor4@g.com', 'PROMOTOR');
+insert into usuario(id, version, username, nombre, apellido, contrasenna, correo, tipo)
+values ("promotor5", 1, 'Promoabalante', 'Juan', 'Cuesta', 'queFollon', 'promotor5@g.com', 'PROMOTOR');
