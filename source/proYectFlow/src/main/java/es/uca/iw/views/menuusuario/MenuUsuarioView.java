e25b3bb3b18e5dc40d05ac116817371d965b9311
Marta <marta.villalopez@alum.uca.es>
2024-12-15 19:35:33 +0100

    Creado menu del promotor
    Creado menu del promotor con todas sus opciones

--- source/proYectFlow/src/main/java/es/uca/iw/views/menuusuario/MenuUsuarioView.java
+++ source/proYectFlow/src/main/java/es/uca/iw/views/menuusuario/MenuUsuarioView.java
@@ -61,12 +61,24 @@ public class MenuUsuarioView extends Main implements HasComponents, HasStyle {
         addCardIfRoleMatches("Nueva solicitud de Proyecto",
                 "Aquí puedes realizar una nueva solicitud de proyecto",
                 "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?q=80&w=2070&auto=format&fit=crop",
-                "Administrar", "registro-proyecto",
+                "Solicitar", "registro-proyecto",
                 List.of("ROLE_SOLICITANTE"));
+
+        addCardIfRoleMatches("Avalar un Proyecto",
+                "Aquí puedes gestionar tus solicitudes de aval de proyectos",
+                "https://images.unsplash.com/photo-1542626991-cbc4e32524cc?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                "Avalar", "proyectosPromotor",
+                List.of("ROLE_PROMOTOR"));
+
+        addCardIfRoleMatches("Tus proyectos avalados",
+                "Aquí puedes ver los proyectos que has avalado",
+                "https://images.unsplash.com/photo-1542626991-cbc4e32524cc?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                "Consultar", "estadomisproyectos",
+                List.of("ROLE_PROMOTOR"));
 
         addCardIfRoleMatches("Datos Personales",
                 "Aquí puedes gestionar tus datos personales",
-                "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?q=80&w=2070&auto=format&fit=crop",
+                "https://images.unsplash.com/photo-1668338857295-7b10bcea47b1?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
                 "Administrar", "Ver-mis-datos",
                 List.of("ROLE_SOLICITANTE", "ROLE_PROMOTOR", "ROLE_CIO", "ROLE_OTP", "ROLE_ADMIN"));
     }
