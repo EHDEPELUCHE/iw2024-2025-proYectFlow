b5f80ff1b36fb9fe91459e15b30c22b1a82ee983
Marta <marta.villalopez@alum.uca.es>
2024-12-29 01:43:58 +0100

    Arreglados fallos en editar proyecto
    Co-Authored-By: Elena Vázquez Rodríguez <100366080+elenavaz@users.noreply.github.com>
    Co-Authored-By: Álvaro Pérez Vargas <80173076+EHDEPELUCHE@users.noreply.github.com>

--- source/proYectFlow/src/main/java/es/uca/iw/Proyecto/views/EditarProyectoSolicitanteView.java
+++ source/proYectFlow/src/main/java/es/uca/iw/Proyecto/views/EditarProyectoSolicitanteView.java
@@ -10,6 +10,7 @@ import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
@@ -40,7 +41,7 @@ import java.util.UUID;
 
 @PageTitle("Editar mis proyectos")
 @Route("EditarProyectoSolicitante")
-@RolesAllowed({"ROLE_SOLICITANTE", "ROLE_PROMOTOR"})
+@RolesAllowed({"ROLE_SOLICITANTE", "ROLE_PROMOTOR", "ROLE_CIO", "ROLE_OTP"})
 
 public class EditarProyectoSolicitanteView extends Composite<VerticalLayout> implements HasUrlParameter<String> {
     Optional<Proyecto> proyecto;
@@ -86,169 +87,175 @@ public class EditarProyectoSolicitanteView extends Composite<VerticalLayout> imp
             //MOSTRAR DATOS DEL PROYECTO
             Proyecto proyectoAux = proyecto.get();
 
-            getContent().add(new H1("Detalles del Proyecto"));
-            VerticalLayout layoutColumn2 = new VerticalLayout();
-            H3 h3 = new H3();
-            FormLayout formLayout2Col = new FormLayout();
-            Optional<Usuario> solicitante = authenticatedUser.get();
-
-            HorizontalLayout layoutRow = new HorizontalLayout();
-            Button buttonGuardar = new Button();
-            Button buttonSecondary = new Button();
-            getContent().setWidth("100%");
-            getContent().getStyle().set("flex-grow", "1");
-            getContent().setJustifyContentMode(FlexComponent.JustifyContentMode.START);
-            getContent().setAlignItems(FlexComponent.Alignment.CENTER);
-            layoutColumn2.setWidth("100%");
-            layoutColumn2.setMaxWidth("800px");
-            layoutColumn2.setHeight("min-content");
-            h3.setText("Registro de proyecto");
-            h3.setWidth("100%");
-            formLayout2Col.setWidth("100%");
-            emailField.setLabel("Solicitante");
-            emailField.setValue(solicitante.get().getCorreo());
-            emailField.setWidth("min-content");
-            emailField.setReadOnly(true);
-            promotor.setLabel("Promotor");
-            promotor.setWidth("min-content");
-
-            promotor.setItems(usuarioService.get(Roles.PROMOTOR));
-            promotor.setItemLabelGenerator(Usuario::getNombre);
-
-            if (proyectoAux.getPromotor() != null) {
-                promotor.setValue(proyectoAux.getPromotor());
-                promotor.setReadOnly(true);
+            if (proyectoAux.getEstado() != Proyecto.Estado.solicitado) {
+                H2 mensajeNoEditable = new H2("En estos momentos ya no es posible editar el proyecto.");
+                getContent().add(mensajeNoEditable);
             } else {
-                promotor.setValue(null);
-                promotor.setReadOnly(false);
-            }
 
-            nombre.setLabel("Nombre del proyecto");
-            nombre.setWidth("min-content");
-            nombre.setValue(proyectoAux.getNombre());
+                getContent().add(new H1("Detalles del Proyecto"));
+                VerticalLayout layoutColumn2 = new VerticalLayout();
+                H3 h3 = new H3();
+                FormLayout formLayout2Col = new FormLayout();
+                Optional<Usuario> solicitante = authenticatedUser.get();
 
-            alcance.setLabel("Alcance");
-            alcance.setWidth("min-content");
-            alcance.setValue(proyectoAux.getAlcance());
+                HorizontalLayout layoutRow = new HorizontalLayout();
+                Button buttonGuardar = new Button();
+                Button buttonSecondary = new Button();
+                getContent().setWidth("100%");
+                getContent().getStyle().set("flex-grow", "1");
+                getContent().setJustifyContentMode(FlexComponent.JustifyContentMode.START);
+                getContent().setAlignItems(FlexComponent.Alignment.CENTER);
+                layoutColumn2.setWidth("100%");
+                layoutColumn2.setMaxWidth("800px");
+                layoutColumn2.setHeight("min-content");
+                h3.setText("Registro de proyecto");
+                h3.setWidth("100%");
+                formLayout2Col.setWidth("100%");
+                emailField.setLabel("Solicitante");
+                emailField.setValue(solicitante.get().getCorreo());
+                emailField.setWidth("min-content");
+                emailField.setReadOnly(true);
+                promotor.setLabel("Promotor");
+                promotor.setWidth("min-content");
 
-            descripcion.setLabel("Descripción");
-            descripcion.setWidth("min-content");
-            descripcion.setValue(proyectoAux.getDescripcion());
+                promotor.setItems(usuarioService.get(Roles.PROMOTOR));
+                promotor.setItemLabelGenerator(Usuario::getNombre);
 
-            interesados.setLabel("Interesados");
-            interesados.setWidth("min-content");
-            interesados.setValue(proyectoAux.getInteresados());
-
-            aportacionInicial.setLabel("Financiación aportada en €");
-            aportacionInicial.setWidth("min-content");
-            aportacionInicial.setValue(proyectoAux.getAportacionInicial());
-
-            coste.setLabel("Coste Total en €");
-            coste.setWidth("min-content");
-            coste.setValue(proyectoAux.getCoste());
-
-            fechaLimite.setPlaceholder("Añadir solo si el proyecto se realiza para cumplimentar alguna ley próxima a entrar en vigor");
-            fechaLimite.setAriaLabel("Añadir solo si el proyecto se realiza para cumplimentar alguna ley próxima a entrar en vigor");
-            if (proyectoAux.getFechaLimite() != null) {
-                fechaLimite.setValue(proyectoAux.getFechaLimite().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
-            }
-
-            upload.setAcceptedFileTypes("application/pdf", ".pdf");
-
-            Button uploadButton = new Button("Upload PDF...");
-            uploadButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-
-            upload.setUploadButton(uploadButton);
-
-            upload.getElement()
-                    .addEventListener("max-files-reached-changed", eventy -> {
-                        boolean maxFilesReached = eventy.getEventData()
-                                .getBoolean("event.detail.value");
-                        uploadButton.setEnabled(!maxFilesReached);
-                    }).addEventData("event.detail.value");
-
-            Button btncancelar = new Button("Volver", eventy -> UI.getCurrent().navigate(EstadoProyectosView.class));
-            btncancelar.addClassName("buttonSecondary");
-
-            Button downloadButton = new Button("Memoria");
-            downloadButton.addClickListener(e -> {
-                byte[] pdfContent = null;
-                try {
-                    pdfContent = proyectoService.getPdf(proyecto.get().getId());
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
+                if (proyectoAux.getPromotor() != null) {
+                    promotor.setValue(proyectoAux.getPromotor());
+                    promotor.setReadOnly(true);
+                } else {
+                    promotor.setValue(null);
+                    promotor.setReadOnly(false);
                 }
-                if (pdfContent != null) {
-                    byte[] finalPdfContent = pdfContent;
-                    StreamResource resource = new StreamResource("Memoria.pdf", () -> new ByteArrayInputStream(finalPdfContent));
-                    Anchor downloadLink = new Anchor(resource, "Download");
-                    downloadLink.getElement().setAttribute("download", true);
-                    downloadLink.getElement().setAttribute("style", "display: none;");
-                    getContent().add(downloadLink);
-                    downloadLink.getElement().callJsFunction("click");
-                    downloadLink.remove();
+
+                nombre.setLabel("Nombre del proyecto");
+                nombre.setWidth("min-content");
+                nombre.setValue(proyectoAux.getNombre());
+
+                alcance.setLabel("Alcance");
+                alcance.setWidth("min-content");
+                alcance.setValue(proyectoAux.getAlcance());
+
+                descripcion.setLabel("Descripción");
+                descripcion.setWidth("min-content");
+                descripcion.setValue(proyectoAux.getDescripcion());
+
+                interesados.setLabel("Interesados");
+                interesados.setWidth("min-content");
+                interesados.setValue(proyectoAux.getInteresados());
+
+                aportacionInicial.setLabel("Financiación aportada en €");
+                aportacionInicial.setWidth("min-content");
+                aportacionInicial.setValue(proyectoAux.getAportacionInicial());
+
+                coste.setLabel("Coste Total en €");
+                coste.setWidth("min-content");
+                coste.setValue(proyectoAux.getCoste());
+
+                fechaLimite.setPlaceholder("Añadir solo si el proyecto se realiza para cumplimentar alguna ley próxima a entrar en vigor");
+                fechaLimite.setAriaLabel("Añadir solo si el proyecto se realiza para cumplimentar alguna ley próxima a entrar en vigor");
+                if (proyectoAux.getFechaLimite() != null) {
+                    fechaLimite.setValue(proyectoAux.getFechaLimite().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
                 }
-            });
 
-            layoutRow.addClassName(LumoUtility.Gap.MEDIUM);
-            layoutRow.setWidth("100%");
-            layoutRow.getStyle().set("flex-grow", "1");
-            layoutRow.setAlignItems(FlexComponent.Alignment.START);
-            layoutRow.setJustifyContentMode(FlexComponent.JustifyContentMode.END);
-            buttonGuardar.setText("Guardar");
-            buttonGuardar.addClickShortcut(Key.ENTER);
-            buttonGuardar.addClickListener(e -> ActualizarProyecto(proyectoAux));
-            buttonGuardar.setWidth("min-content");
-            buttonGuardar.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-            buttonSecondary.setText("Cancelar");
-            layoutRow.setAlignSelf(FlexComponent.Alignment.START, btncancelar);
-            buttonSecondary.setWidth("min-content");
-            getContent().add(layoutColumn2);
-            layoutColumn2.add(h3);
-            layoutColumn2.add(formLayout2Col);
-            formLayout2Col.add(emailField);
-            formLayout2Col.add(promotor);
-            formLayout2Col.add(nombre);
-            formLayout2Col.add(descripcion);
-            formLayout2Col.add(interesados);
-            formLayout2Col.add(alcance);
-            formLayout2Col.add(aportacionInicial);
-            formLayout2Col.add(coste);
-            formLayout2Col.add(fechaLimite);
-            formLayout2Col.add(upload);
+                upload.setAcceptedFileTypes("application/pdf", ".pdf");
 
-            buttonGuardar.addClickListener(eventG -> {
+                Button uploadButton = new Button("Upload PDF...");
+                uploadButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
 
-                Notification notification = Notification.show("Los datos del proyecto han sido actualizados.");
+                upload.setUploadButton(uploadButton);
 
-                notification.setDuration(2000);
-                notification.addDetachListener(detachEvent -> {
-                    UI.getCurrent().navigate("estadomisproyectos");
+                upload.getElement()
+                        .addEventListener("max-files-reached-changed", eventy -> {
+                            boolean maxFilesReached = eventy.getEventData()
+                                    .getBoolean("event.detail.value");
+                            uploadButton.setEnabled(!maxFilesReached);
+                        }).addEventData("event.detail.value");
+
+                Button btncancelar = new Button("Volver", eventy -> UI.getCurrent().navigate(EstadoProyectosView.class));
+                btncancelar.addClassName("buttonSecondary");
+
+                Button downloadButton = new Button("Memoria");
+                downloadButton.addClickListener(e -> {
+                    byte[] pdfContent = null;
+                    try {
+                        pdfContent = proyectoService.getPdf(proyecto.get().getId());
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    if (pdfContent != null) {
+                        byte[] finalPdfContent = pdfContent;
+                        StreamResource resource = new StreamResource("Memoria.pdf", () -> new ByteArrayInputStream(finalPdfContent));
+                        Anchor downloadLink = new Anchor(resource, "Download");
+                        downloadLink.getElement().setAttribute("download", true);
+                        downloadLink.getElement().setAttribute("style", "display: none;");
+                        getContent().add(downloadLink);
+                        downloadLink.getElement().callJsFunction("click");
+                        downloadLink.remove();
+                    }
                 });
-            });
 
-            Button Borrar = new Button("Borrar Proyecto", eventy -> {
-                if(proyectoAux.getEstado() != Proyecto.Estado.solicitado){
-                    proyectoService.delete(proyectoAux.getId());
-                    UI.getCurrent().navigate(ProyectosView.class);
-                }else{
-                    Notification.show("Este proyecto ya no se puede eliminar");
-                }
-            });
-            Borrar.addClassName("button-danger");
-            Borrar.addThemeVariants(ButtonVariant.LUMO_ERROR);
+                layoutRow.addClassName(LumoUtility.Gap.MEDIUM);
+                layoutRow.setWidth("100%");
+                layoutRow.getStyle().set("flex-grow", "1");
+                layoutRow.setAlignItems(FlexComponent.Alignment.START);
+                layoutRow.setJustifyContentMode(FlexComponent.JustifyContentMode.END);
+                buttonGuardar.setText("Guardar");
+                buttonGuardar.addClickShortcut(Key.ENTER);
+                buttonGuardar.addClickListener(e -> ActualizarProyecto(proyectoAux));
+                buttonGuardar.setWidth("min-content");
+                buttonGuardar.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+                buttonSecondary.setText("Cancelar");
+                layoutRow.setAlignSelf(FlexComponent.Alignment.START, btncancelar);
+                buttonSecondary.setWidth("min-content");
+                getContent().add(layoutColumn2);
+                layoutColumn2.add(h3);
+                layoutColumn2.add(formLayout2Col);
+                formLayout2Col.add(emailField);
+                formLayout2Col.add(promotor);
+                formLayout2Col.add(nombre);
+                formLayout2Col.add(descripcion);
+                formLayout2Col.add(interesados);
+                formLayout2Col.add(alcance);
+                formLayout2Col.add(aportacionInicial);
+                formLayout2Col.add(coste);
+                formLayout2Col.add(fechaLimite);
+                formLayout2Col.add(upload);
 
-            layoutColumn2.add(layoutRow);
-            layoutRow.add(Borrar);
-            layoutRow.add(downloadButton);
-            layoutRow.add(buttonGuardar);
-            layoutRow.add(btncancelar);
-            binder.bindInstanceFields(this);
-            binder.setBean(proyectoAux);
+                buttonGuardar.addClickListener(eventG -> {
+
+                    Notification notification = Notification.show("Los datos del proyecto han sido actualizados.");
+
+                    notification.setDuration(2000);
+                    notification.addDetachListener(detachEvent -> {
+                        UI.getCurrent().navigate("estadomisproyectos");
+                    });
+                });
+
+                Button Borrar = new Button("Borrar Proyecto", eventy -> {
+                    if (proyectoAux.getEstado() != Proyecto.Estado.solicitado) {
+                        proyectoService.delete(proyectoAux.getId());
+                        UI.getCurrent().navigate(ProyectosView.class);
+                    } else {
+                        Notification.show("Este proyecto ya no se puede eliminar");
+                    }
+                });
+                Borrar.addClassName("button-danger");
+                Borrar.addThemeVariants(ButtonVariant.LUMO_ERROR);
+
+                layoutColumn2.add(layoutRow);
+                layoutRow.add(Borrar);
+                layoutRow.add(downloadButton);
+                layoutRow.add(buttonGuardar);
+                layoutRow.add(btncancelar);
+                binder.bindInstanceFields(this);
+                binder.setBean(proyectoAux);
+            }
         }
     }
     private void ActualizarProyecto(Proyecto proyectoAux) {
-        if(proyectoAux.getEstado() != Proyecto.Estado.denegado) proyectoService.update(proyectoAux);
+        if(proyectoAux.getEstado() != Proyecto.Estado.solicitado) proyectoService.update(proyectoAux);
         else Notification.show("Este proyecto no se puede editar");
     }
 }
--- source/proYectFlow/src/main/java/es/uca/iw/Usuario/views/MenuUsuarioView.java
+++ source/proYectFlow/src/main/java/es/uca/iw/Usuario/views/MenuUsuarioView.java
@@ -79,17 +79,17 @@ public class MenuUsuarioView extends Main implements HasComponents, HasStyle {
                 "Consultar", "estadomisproyectos",
                 List.of("ROLE_PROMOTOR"));
 
-        addCardIfRoleMatches("Evaluación de proyectos",
-                "Aquí puedes evaluar los proyectos pendientes",
-                "https://images.unsplash.com/photo-1452457750107-cd084dce177d?q=80&w=2001&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                "Evaluar", "proyectosOTP",
-                List.of("ROLE_OTP"));
-
         addCardIfRoleMatches("Proyectos en desarrollo",
                 "Aquí puedes ver todos los proyectos en desarrollo",
                 "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?q=80&w=2070&auto=format&fit=crop",
                 "Consultar", "ProyectosDesarrollo",
                 null);
+
+        addCardIfRoleMatches("Evaluación de proyectos",
+                "Aquí puedes evaluar los proyectos pendientes",
+                "https://images.unsplash.com/photo-1452457750107-cd084dce177d?q=80&w=2001&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                "Evaluar", "proyectosOTP",
+                List.of("ROLE_OTP"));
 
         addCardIfRoleMatches("Historial de proyectos",
                 "Aquí puedes consultar todos los proyectos",
