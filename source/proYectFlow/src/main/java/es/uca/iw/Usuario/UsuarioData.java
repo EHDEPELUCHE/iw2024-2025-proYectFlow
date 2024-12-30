6c042dae54009d008200c5869bbd2085e4c9fe3a
Marta <marta.villalopez@alum.uca.es>
2024-12-30 18:21:46 +0100

    Division de Data en 3 clases
    Ahora UsuarioData, ProyectoData y ConvocatoriaData
    
    Co-Authored-By: Elena Vázquez Rodríguez <100366080+elenavaz@users.noreply.github.com>
    Co-Authored-By: Álvaro Pérez Vargas <80173076+EHDEPELUCHE@users.noreply.github.com>

--- source/proYectFlow/src/main/java/es/uca/iw/Usuario/UsuarioData.java
+++ source/proYectFlow/src/main/java/es/uca/iw/Usuario/UsuarioData.java
@@ -1,25 +1,18 @@
 package es.uca.iw.Usuario;
 
-import es.uca.iw.Convocatoria.Convocatoria;
-import es.uca.iw.Convocatoria.ConvocatoriaRepository;
-import es.uca.iw.Convocatoria.ConvocatoriaService;
-import es.uca.iw.Proyecto.Proyecto;
-import es.uca.iw.Proyecto.ProyectoRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import es.uca.iw.global.Roles;
 
-import java.math.BigDecimal;
-import java.text.SimpleDateFormat;
 import java.util.List;
 
-@Component
+@Component("usuarioData")
 public class UsuarioData {
 
     @Bean
-    CommandLineRunner initData(UsuarioRepository usuarioRepository, ProyectoRepository proyectoRepository, ConvocatoriaRepository convocatoriaRepository, PasswordEncoder passwordEncoder, ConvocatoriaService convocatoriaService) {
+    CommandLineRunner initData(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder) {
         return args -> {
             if (usuarioRepository.count() == 0) {
                 List<Usuario> usuarios = List.of(
@@ -40,46 +33,6 @@ public class UsuarioData {
                         );
                 usuarios.forEach(usuario -> usuario.setActivo(true));
                 usuarioRepository.saveAll(usuarios);
-            }
-
-            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
-            if (convocatoriaRepository.count() == 0) {
-                List<Convocatoria> convocatorias = List.of(
-                        new Convocatoria(new BigDecimal("1000000"), sdf.parse("15/02/2025"),
-                                sdf.parse("15/09/2024"), sdf.parse("15/06/2025")),
-                        new Convocatoria(new BigDecimal("750000"), sdf.parse("15/02/2024"),
-                                sdf.parse("15/09/2023"), sdf.parse("15/06/2024")),
-                        new Convocatoria(new BigDecimal("500000"), sdf.parse("15/02/2023"),
-                                sdf.parse("15/09/2022"), sdf.parse("15/06/2023"))
-                );
-
-                convocatorias.get(0).setActiva(true);
-                convocatorias.get(1).setActiva(false);
-                convocatorias.get(2).setActiva(false);
-                convocatoriaRepository.saveAll(convocatorias);
-            }
-
-            if (proyectoRepository.count() == 0) {
-                Usuario promotor = usuarioRepository.findByUsername("maria");
-                Usuario solicitante = usuarioRepository.findByUsername("lucia");
-                Convocatoria convocatoriaActiva = convocatoriaService.ConvocatoriaActual();
-
-                if (promotor != null && solicitante != null && convocatoriaActiva != null) {
-                    List<Proyecto> proyectos = List.of(
-                            new Proyecto("ProyectoEjemplo1", "Descripción1", "Interesados1",
-                                    "Alcance1", new BigDecimal("10000"), new BigDecimal("150000"),
-                                    promotor, solicitante, null, null),
-                            new Proyecto( "ProyectoEjemplo2", "Descripción2", "Interesados2",
-                                    "Alcanc2", new BigDecimal("20000"), new BigDecimal("250000"),
-                                    promotor, solicitante, sdf.parse("1/09/2025"), null ),
-                            new Proyecto( "ProyectoEjemplo3", "Descripción3", "Interesados3",
-                                    "Alcance3", new BigDecimal("30000"), new BigDecimal("350000"),
-                                    promotor, solicitante, sdf.parse("5/08/2025"), null )
-                    );
-                    proyectos.forEach(proyecto -> proyecto.setConvocatoria(convocatoriaActiva));
-                    proyectoRepository.saveAll(proyectos);
-                }
             }
         };
     }
