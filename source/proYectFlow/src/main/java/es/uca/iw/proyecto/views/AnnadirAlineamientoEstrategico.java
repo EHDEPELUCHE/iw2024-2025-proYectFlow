a0611a714b0d1ededbf0c93d0f503275a1c1a4a3
Álvaro Pérez Vargas <80173076+EHDEPELUCHE@users.noreply.github.com>
2025-01-09 12:45:36 +0100

    Tildes que se nos habían pasado
    Co-Authored-By: Elena Vázquez Rodríguez <100366080+elenavaz@users.noreply.github.com>
    Co-Authored-By: Marta <96750141+martav2@users.noreply.github.com>

--- source/proYectFlow/src/main/java/es/uca/iw/proyecto/views/AnnadirAlineamientoEstrategico.java
+++ source/proYectFlow/src/main/java/es/uca/iw/proyecto/views/AnnadirAlineamientoEstrategico.java
@@ -3,8 +3,11 @@ package es.uca.iw.proyecto.views;
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dependency.Uses;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -28,26 +31,31 @@ public class AnnadirAlineamientoEstrategico extends Composite<VerticalLayout> {
 
     public AnnadirAlineamientoEstrategico(AlineamientoEstrategicoService alineamientoEstrategicoService) {
         this.alineamientoEstrategicoService = alineamientoEstrategicoService;
-       
+
         TextArea objetivo = new TextArea("Objetivo nuevo");
         Button btnAceptar = new Button("Aceptar");
+        btnAceptar.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         btnAceptar.addClickShortcut(Key.ENTER);
-        H1 h1 = new H1("Añadir Alineamiento Estrategico");
+        H1 h1 = new H1("Añadir Alineamiento Estratégico");
         btnAceptar.addClickListener(e -> {
             AlineamientoEstrategico alineamientoEstrategico = new AlineamientoEstrategico();
-            if(!Objects.equals(objetivo.getValue(), objetivo.getEmptyValue())){
+            if (!Objects.equals(objetivo.getValue(), objetivo.getEmptyValue())) {
                 alineamientoEstrategico.setObjetivo(objetivo.getValue());
-                try{
+                try {
                     alineamientoEstrategicoService.guardar(alineamientoEstrategico);
                     Notification.show("Objetivo añadido correctamente");
                     objetivo.clear();
                 } catch (Exception ex) {
                     Notification.show("Los datos introducidos son erróneos: " + ex.getMessage());
                 }
-            }else{
+            } else {
                 Notification.show("El objetivo no puede estar vacio");
             }
         });
-        getContent().add(h1, objetivo, btnAceptar);
+        H2 tabla = new H2("Alineamientos Estratégicos");
+        Grid<AlineamientoEstrategico> grid = new Grid<>(AlineamientoEstrategico.class, false);
+        grid.addColumn(AlineamientoEstrategico::getObjetivo);
+        grid.setItems(alineamientoEstrategicoService.findAll());
+        getContent().add(h1, objetivo, btnAceptar, tabla, grid);
     }
 }
